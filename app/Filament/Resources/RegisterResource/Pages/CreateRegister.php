1e23aebdb70293e3e68ec6a8dc808c2d297cac76
Victor Languer <victorlanguer@hotmail.com>
2025-04-09 19:07:43 -0300

    Updated Create and Edit pages to redirect to index after created/edited

--- app/Filament/Resources/RegisterResource/Pages/CreateRegister.php
+++ app/Filament/Resources/RegisterResource/Pages/CreateRegister.php
@@ -5,8 +5,14 @@ namespace App\Filament\Resources\RegisterResource\Pages;
 use App\Filament\Resources\RegisterResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Support\Facades\Storage;
 
 class CreateRegister extends CreateRecord
 {
     protected static string $resource = RegisterResource::class;
+
+    protected function getRedirectUrl(): string
+    {
+        return RegisterResource::getUrl('index');
+    }
 }
--- app/Filament/Resources/RegisterResource/Pages/EditRegister.php
+++ app/Filament/Resources/RegisterResource/Pages/EditRegister.php
@@ -17,4 +17,9 @@ class EditRegister extends EditRecord
             Actions\DeleteAction::make(),
         ];
     }
+
+    protected function getRedirectUrl(): string
+    {
+        return RegisterResource::getUrl('index');
+    }
 }
